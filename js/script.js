774ffb3182e82d030ae61338a5a02b506bf4973e
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-12-08 17:04:48 -0700

    Finish quotes and add tags

--- js/script.js
+++ js/script.js
@@ -14,37 +14,56 @@ project 1 - A Random Quote Generator
 const quotes = [
 	{
 		quote: 'Java is to JavaScript what car is to carpet.',
-		source: 'Chris Heilmann'
+		source: 'Chris Heilmann',
+		tags: ['tech', 'code']
 	},
 	{
 		quote: 'Before software can be reusable it first has to be usable.',
-		source: 'Ralph Johnson'
+		source: 'Ralph Johnson',
+		tags: ['tech', 'code']
 	},
 	{
 		quote: `Code is like humor. When you have to explain it, it's bad`,
-		source: 'Cory House'
+		source: 'Cory House',
+		tags: ['tech', 'code']
 	},
 	{
-		quote:
-			'The future belongs to those who believe in the beauty of their dreams.',
-		source: 'Eleanor Roosevelt'
+		quote: 'No one can make you feel inferior without your consent.',
+		source: 'Eleanor Roosevelt',
+		citation: 'This is My Story',
+		tags: ['motivation', 'inspiration', 'wisdom']
 	},
 	{
 		quote:
 			'Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.',
 		source: 'Albert Einstein',
 		citation:
-			'What Life Means to Einstein: An Interview by George Sylvester Viereck'
+			'What Life Means to Einstein: An Interview by George Sylvester Viereck',
+		tags: ['motivation', 'inspiration', 'historic']
 	},
 	{
 		quote: `I'm a success today because I had a friend who believed in me and I didn't have the heart to let him down.`,
-		source: 'Abraham Lincoln'
+		source: 'Abraham Lincoln',
+		tags: ['motivation', 'inspiration', 'historic']
 	},
 	{
 		quote:
 			'I know I have but the body of a weak and feeble woman; but I have the heart of a king...',
 		source: 'Elizabeth I',
-		year: 1588
+		year: 1588,
+		tags: ['motivation', 'historic']
+	},
+	{
+		quote: 'Be the change that you wish to see in the world.',
+		source: 'Mahatma Gandhi',
+		tags: ['change', 'inspiration']
+	},
+	{
+		quote:
+			'Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate; only love can do that.',
+		source: 'Dr. Martin Luther King Jr.',
+		citation: 'A Testament of Hope: The Essential Writings and Speeches',
+		tags: ['inspiration', 'peace', 'historic']
 	}
 ];
 
